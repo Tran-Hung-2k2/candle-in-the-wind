c3c61f91da0c38bcf08bfa4520fb6b2ab492fa8b
Tran Hung <tranviethung912002@gmail.com>
2023-03-25 21:34:33 +0700

    .

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,23 +1,26 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv"
-import cookieParser from "cookie-parser";
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 import route from './routes/index.js';
 
 dotenv.config();
+const PORT = process.env.PORT || 8000;
+
 const app = express();
 
-app.use(cors({
+app.use(
+  cors({
     origin: 'http://localhost:3001',
-    credentials: true
-}));
+    credentials: true,
+  }),
+);
 app.use(cookieParser());
 app.use(express.json());
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 route(app);
 
-const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-})
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
